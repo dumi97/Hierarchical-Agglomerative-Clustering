56b6491984bb122eb5de27ca5619760ce0ccc2d7
dumi97 <giboz97@interia.pl>
2020-04-17 14:41:06 +0200

    Add project files.

---
+++ Hierarchical Agglomerative Clustering/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hierarchical_Agglomerative_Clustering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
