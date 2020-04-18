adeb4dfd34aeea13ddf5ef2e7efdcc7099f1f748
dumi97 <giboz97@interia.pl>
2020-04-18 16:42:19 +0200

    Start working on command line args

--- Hierarchical Agglomerative Clustering/Program.cs
+++ Hierarchical Agglomerative Clustering/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Mono.Options;
 
 namespace Hierarchical_Agglomerative_Clustering
 {
@@ -7,17 +8,84 @@ namespace Hierarchical_Agglomerative_Clustering
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool showHelp = true;
+            string linkageMethod = "average", distanceMethod = "euclidean2";
+            bool generateData = false;
+            string generatedFileName = "";
+            double minGen = 0, maxGen = 100;
+            int genCount = 6;
+            string inputFile = "input.txt";
+            string outputFile = "output.txt";
+
+            var p = new OptionSet() {
+                { "l|linkage=", "(string) the linkage method of the algorithm\n" +
+                    "possible valuse: minimum, maximum, average\n" +
+                    "default: average",
+                    (string v) => linkageMethod = v },
+                { "d|distance=", "(string) the distance method of the algorithm\n" +
+                    "possible valuse: euclidean, euclidean2 (or euclideansquared), manhattan\n" +
+                    "default: euclidean2",
+                    (string v) => distanceMethod = v },
+                { "g|generate", "generate input data",
+                    v => generateData = v != null },
+                { "f|generatedName=", "(string) name of the file to save the generated input data\n" +
+                    "empty string means no file will be generated\n" +
+                    "default: <empty>",
+                    (string v) => generatedFileName = v },
+                { "n|minGenerated=", "(double) minimum value of the generated data\n" +
+                    "default: 0.0",
+                    (double v) => minGen = v },
+                { "m|maxGenerated=", "(double) maximum value of the generated data\n" +
+                    "default: 100.0",
+                    (double v) => maxGen = v },
+                { "c|generatedCount=", "(int) how many data point to generate\n" +
+                    "default: 6",
+                    (int v) => genCount = v },
+                { "i|input=", "(string) the input file\n" +
+                    "default: input.txt",
+                    (string v) => inputFile = v },
+                { "o|output=", "(string) the output file\n" +
+                    "default: output.txt",
+                    (string v) => outputFile = v },
+                { "h|help",  "show this message and exit",
+                    v => showHelp = v != null },
+            };
+
+            List<string> extra;
+            try
+            {
+                extra = p.Parse(args);
+            }
+            catch (OptionException e)
+            {
+                Console.Write("Hierarchical Agglomerative Clustering: ");
+                Console.WriteLine(e.Message);
+                Console.WriteLine("Try 'hac.exe --help' for more information.");
+                return;
+            }
+
+            if(showHelp)
+            {
+                //TODO
+                return;
+            }
+
             List<Cluster> outputList;
             DataIO dio = new DataIO();
             HAC hac = new HAC();
 
-            //List<Point> testList = new List<Point>();
-            //testList = dio.LoadData(@"input.txt");
-            //dio.SaveData(testList);
+            // load data
+            List<Point> input = null;
+            if (generateData)
+                input = dio.GenerateData(minGen, maxGen, genCount, fileName: generatedFileName);
+            else
+                input = dio.LoadData(inputFile);
 
-            outputList = hac.ClusterData(dio.GenerateData(count:4,fileName:"generated.txt"));
-            dio.SaveData(outputList);
+            // cluster data
+            outputList = hac.ClusterData(input, linkageMethod, distanceMethod);
+
+            // output data
+            dio.SaveData(outputList, outputFile);
         }
     }
 }
